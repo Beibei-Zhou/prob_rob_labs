2fcd3e222c2bcda47d071e7607ae9da8da1c561b
Hang Zhou <hz2916@columbia.edu>
2024-11-08 11:40:24 -0500

    Lab4 Ass1-3

--- labs/src/ground_truth_extraction/ground_truth_extraction.py
+++ labs/src/ground_truth_extraction/ground_truth_extraction.py
@@ -1,59 +1,59 @@
 #!/usr/bin/env python
 
 import rospy
-#from std_msgs.msg import Float64, Empty
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from gazebo_msgs.msg import LinkStates
 from std_msgs.msg import Header
 
 class Ground_Truth:
     def __init__(self):
-        self.ground_truth_sub = rospy.Subscriber('/gazebo/link_states', LinkStates, self.ground_truth_callback)
-        self.ground_truth_Pose_pub = rospy.Publisher('/jackal/ground_truth/pose', PoseStamped, queue_size=1)
-        self.ground_truth_Twist_pub = rospy.Publisher('/jackal/ground_truth/twist', TwistStamped, queue_size=1)
+        self.ground_truth_sub = rospy.Subscriber('/gazebo/link_states', LinkStates, self.ground_truth_callback, queue_size=10)
+        self.ground_truth_Pose_pub = rospy.Publisher('/jackal/ground_truth/pose', PoseStamped, queue_size=10)
+        self.ground_truth_Twist_pub = rospy.Publisher('/jackal/ground_truth/twist', TwistStamped, queue_size=10)
         self.pose_stamp = None
         self.twist_stamp = None
-        self.rate = rospy.Rate(10)
-        self.index = 1
-    
+        self.index = None  # Initialize index as None
+
     def ground_truth_callback(self, msg):
-        for i in range(len(msg.name)):
-            #rospy.loginfo("Message name {}".format(msg.name[i]))
-            if msg.name[i] == "jackal::base_link":
-                break
-        
-        self.index = i
-        rospy.loginfo("Link Index {}".format(i))
-        current = rospy.get_rostime()
+        if self.index is None:
+            try:
+                self.index = msg.name.index("jackal::base_link")
+                rospy.loginfo("Found jackal::base_link at index {}".format(self.index))
+            except ValueError:
+                rospy.logwarn("jackal::base_link not found in link_states")
+                return  # Cannot proceed without index
+
+        current = rospy.Time.now()
+
         self.pose_stamp = PoseStamped(
-            header = Header(
-                stamp = current,
-                frame_id = 'odom'
+            header=Header(
+                stamp=current,
+                frame_id='odom'
             ),
-            pose = msg.pose[self.index]
+            pose=msg.pose[self.index]
         )
 
         self.twist_stamp = TwistStamped(
-            header = Header(
-                stamp = current,
-                frame_id = 'odom'
+            header=Header(
+                stamp=current,
+                frame_id='odom'
             ),
-            twist = msg.twist[self.index]
+            twist=msg.twist[self.index]
         )
 
         self.ground_truth_Pose_pub.publish(self.pose_stamp)
-        
         self.ground_truth_Twist_pub.publish(self.twist_stamp)
+
+        # Reduce logging to prevent delays
         rospy.loginfo("Publish Pose {}".format(self.pose_stamp))
         rospy.loginfo("Publish Twist {}".format(self.twist_stamp))
-        
+
 def main():
-    rospy.init_node('ground_truth_extraction')
+    rospy.init_node('ground_truth_extraction', anonymous=True)
     GT_find = Ground_Truth()
-    GT_find.ground_truth_callback
-    rospy.loginfo('starting ground_truth_extraction')
+    rospy.loginfo('Starting ground_truth_extraction')
     rospy.spin()
-    rospy.loginfo('done')
+    rospy.loginfo('Shutting down')
 
 if __name__=='__main__':
     main()
